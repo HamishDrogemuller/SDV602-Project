a528d5cbaae20df8c95b0cdf014678d1a17d3e62
HamishDrogemuller <79230862+HamishDrogemuller@users.noreply.github.com>
2022-08-08 10:54:08 +1200

    Australia Indeed Scraper

--- .Project/scraper.py
+++ .Project/scraper.py
@@ -14,7 +14,7 @@ Combine all into .csv for project []
 """
 
 # Extract- NZ Indeed(Software Developer)
-def extract(page):
+def extractnz(page):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     urlNZ = f"https://nz.indeed.com/jobs?q=software%20developer&l=New%20Zealand&vjk=118731a217acab33&start={page}"
     r = requests.get(urlNZ, headers)
@@ -24,13 +24,68 @@ def extract(page):
 
 
 #Transform
-def transform(soup):
+def transformnz(soup):
     divs = soup.find_all('div', class_='cardOutline')
     for item in divs:
         title = item.find('a').text.strip()
-        company = item.find('span', class_='companyName').text.strip()
-        location = item.find('div', class_='companyLocation').text.strip().replace('\n', '')
-        summary = item.find('div', class_='job-snippet').text.strip()
+        try:
+            company = item.find('span', class_='companyName').text.strip()
+        except AttributeError:
+            company = 'N/A'
+        try:
+            location = item.find('div', class_='companyLocation').text.strip().replace('\n', '')
+        except AttributeError:
+            locaton = 'N/A'
+        try:
+            summary = item.find('div', class_='job-snippet').text.strip()
+        except AttributeError:
+            summary = 'N/A'
+        job = {
+            'Title' : title,
+            'Company' : company,
+            'Location' : location,
+            'Summary' : summary
+        }
+        jobListnz.append(job)
+    return
+
+jobListnz = []
+
+for i in range(0, 40, 10):
+    print(f'Getting Page, {i}')
+    c = extractnz(0)
+    transformnz(c)
+
+df = pd.DataFrame(jobListnz)
+
+print(df.head())
+
+df.to_csv('NZSD.csv') #NZ Software Dev Jobs
+
+#Extract - Au Indeed(Software Developer)
+def extractau(page):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    urlAU = f"https://au.indeed.com/jobs?q=Software%20Developer&start={page}&vjk=7fbac8aeaba92384"
+    r = requests.get(urlAU, headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
+
+def transformau(soup):
+    divs = soup.find_all('div', class_='cardOutline')
+    for item in divs:
+        title = item.find('a').text.strip()
+        try:
+            company = item.find('span', class_='companyName').text.strip()
+        except AttributeError:
+            company = 'N/A'
+        try:
+            location = item.find('div', class_='companyLocation').text.strip().replace('\n', '')
+        except AttributeError:
+            locaton = 'N/A'
+        try:
+            summary = item.find('div', class_='job-snippet').text.strip()
+        except AttributeError:
+            summary = 'N/A'
         
         job = {
             'Title' : title,
@@ -38,20 +93,19 @@ def transform(soup):
             'Location' : location,
             'Summary' : summary
         }
-        jobList.append(job)
+        jobListau.append(job)
     return
 
-jobList = []
+jobListau = []
 
-for i in range(0, 60, 10):
+for i in range(0, 40, 10):
     print(f'Getting Page, {i}')
-    c = extract(0)
-    transform(c)
+    c = extractau(0)
+    transformau(c)
 
-df = pd.DataFrame(jobList)
+dfau = pd.DataFrame(jobListau)
 
-print(df.head())
+print(dfau.head())
 
-df.to_csv('NZSD.csv') #NZ Software Dev Jobs
+dfau.to_csv('AUSD.csv')
 
-#Extract - Au Indeed(Software Developer)
