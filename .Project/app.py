272e0c646793cdaec0243b52e09163a1e7b701cf
HamishDrogemuller <79230862+HamishDrogemuller@users.noreply.github.com>
2022-10-28 15:06:37 +1300

    updating structure

--- .Project/app.py
+++ .Project/app.py
@@ -1,7 +1,7 @@
 """# Invoking start script/function for user to begin the proccess of using the application.  
 """
 #import view.login as login
-from view import login as login
+from view import login_view as login
 
 if __name__ == "__main__":
     login.login_main()
